78445f1e478edf722099facd55722adc0ae60e10
Ludivine Bocquier <98520169+ludivineboc@users.noreply.github.com>
2023-04-06 17:57:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+var urlcourante = document.location.href;  
+
+// Supprimons l'éventuel dernier slash de l'URL
+var urlcourante = urlcourante.replace(/\/$/, "");
+var queue_url = urlcourante.substring (urlcourante.lastIndexOf( "/" )+1 );
+
+
+const url = new URL(window.location.href);
+const searchParams = new URLSearchParams(url.search);
+
+const id = searchParams.get('id');
+console.log(id)
+
+
+var urlFixe = "https://bnwwkshop.glitch.me/"
+document.getElementById("lien-bq").href = urlFixe + id;
+document.getElementById("lien-hq").href = urlFixe + id;
+
+
+
+
+///sticky header /// 
+
+window.onscroll = function() {myFunction()};
+
+
+var fix = document.getElementById("myFix");
+
+
+
+var sticky = fix.offsetTop;
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    fix.classList.add("sticky");
+  } else {
+    fix.classList.remove("sticky");
+
+  }
+} 
+
+///// return button lecteur //// 
+document.getElementById("go-back").addEventListener("click", () => {
+  history.back();
+});
